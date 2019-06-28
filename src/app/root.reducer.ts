0ecdcdc65de0bce369689ba2155532fc3512c2d6
Kunal  Kamble <kunal.kamble@CALSOFTHQ.COM>
2019-06-28 11:08:33 +0530

    [refactor] moved closer to the store

--- src/app/root.reducer.ts
+++
@@ -1,5 +0,0 @@
-import { cartReducer } from "./store/cart/cart.reducers";
-import { StoreModule } from "@ngrx/store";
-import { inventory } from './store/inventory/inventory.reducers';
-
-export const rootReducer = StoreModule.forRoot({ cart: cartReducer, inventory });
